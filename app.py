2bde1b52f6a8b6c83150103e99c642ac7ee1c415
yktind <yk7129388@gmail.com>
2025-04-14 18:49:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,6 +86,6 @@ def index():
 
     return render_template_string(HTML_TEMPLATE, status=status)
 
-if __name__ == "__main__":
-    app.run(debug=True)
-        
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
