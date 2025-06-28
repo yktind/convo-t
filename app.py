63504675bb11d89eb70a7c12bc24916e0c1a2092
yktind <yk7129388@gmail.com>
2025-06-28 08:17:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -164,6 +164,6 @@ def stop():
     is_running = False
     return redirect("/")
 
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
     
